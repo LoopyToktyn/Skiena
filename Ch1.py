a914153725158248529e26748a092067e2719df5
Jared Wolterstorff <jstorff@gmail.com>
2023-07-12 00:46:57 -0500

    undo some rearranging that caused a bug, add some functions to generate point sets and calc and compare averages

--- Ch1.py
+++ Ch1.py
@@ -3,14 +3,6 @@ import math
 
 LIST_A = [(1,1),(2,1),(3,1)]
 
-def dist(dimensions, pointA, pointB):
-    if len(pointA) != len(pointB):
-        raise ValueError("Both points must have same number of dimensions")
-    if len(pointA) != dimensions:
-        raise ValueError("Number of dimensions must match the points")
-
-    return math.sqrt(sum((pointA[i] - pointB[i])**2 for i in range(dimensions)))
-
 def nearest_neighbor(points):
     # Pick a random starting point
     p0 = random.choice(points)
@@ -25,9 +17,8 @@ def nearest_neighbor(points):
         points.remove(nearest)
         path.append(nearest)
 
-    # connect back to p0 (calculate distance between last point found and p0)
-    # final_dist = dist(len(p0),p0,path[-1])
-    # path[0]["dist"] = final_dist
+    # connect beginning to end
+    path.append(path[0])
 
     return path
 
@@ -52,7 +43,10 @@ def closest_pair(points):
     # Repeat until list of lists contains one single list
     while (len(cList) > 1):
         cList = connect_chains(cList)
-    
+
+    # connect beginning to end
+    cList[0].append(cList[0][0])
+
     return cList[0]
         
 
@@ -92,15 +86,64 @@ def find_nearest_chain(chain,chain_list):
 
 
 
+def dist(dimensions, pointA, pointB):
+    if len(pointA) != len(pointB):
+        raise ValueError("Both points must have same number of dimensions")
+    if len(pointA) != dimensions:
+        raise ValueError("Number of dimensions must match the points")
 
+    return math.sqrt(sum((pointA[i] - pointB[i])**2 for i in range(dimensions)))
 
 # result = nearest_neighbor(LIST_A.copy())
 # print(result)
 
-for i in range(10):
-    result = closest_pair(LIST_A.copy())
-    print(result)
+def generate_point_list(dimensions,num_points,min_val,max_val):
+    # num_points = random.randint(minPoint,maxPoints)
+    points = set()
+    while len(points) < num_points:
+        points.add(generate_point(dimensions,min_val,max_val))
+    return list(points)
+
+def generate_point(dimensions,min,max):
+    return tuple(random.randint(min,max) for _ in range(dimensions))
 
 
+def get_path_dist(dimensions,path):
+    total_dist = 0
+    for i in range(len(path)-1):
+        total_dist += dist(dimensions,path[i],path[i+1])
+    return total_dist
+
+
+# for _ in range(5):
+#     print(generate_point_list(4,5,10,-5,5))
+
+
+DIMENSIONS = 4
+NUM_POINTS = 10
+MIN_VAL = -8
+MAX_VAL = 8
+ITERATIONS = 100
+print("CLOSEST PAIR")
+c_dists = []
+for _ in range(ITERATIONS):
+    result = closest_pair(generate_point_list(DIMENSIONS,NUM_POINTS,MIN_VAL,MAX_VAL))
+    # print(result)
+    path_dist = get_path_dist(DIMENSIONS,result)
+    c_dists.append(path_dist)
+    # print("dist: " + str(path_dist))
+
+print("")
+print("NEAREST NEIGHBOR")
+p_dists = []
+for _ in range(ITERATIONS):
+    result = nearest_neighbor(generate_point_list(DIMENSIONS,NUM_POINTS,MIN_VAL,MAX_VAL))
+    # print(result)
+    path_dist = get_path_dist(DIMENSIONS,result)
+    p_dists.append(path_dist)
+    # print("dist: " + str(path_dist))
+
+print("Average closest pair: " + str(sum(c_dists) / len(c_dists)))
+print("Average nearest neighbor: " + str(sum(p_dists) / len(p_dists)))
 
 

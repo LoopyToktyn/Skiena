3a8e2952f69285acd3724828c2fd209402f0a3ae
Jared Wolterstorff <jstorff@gmail.com>
2023-07-16 22:19:33 -0500

    add super fast solution

--- src/ch2/countBits.py
+++ src/ch2/countBits.py
@@ -19,6 +19,17 @@ def countBits(n: int) -> List[int]:
 def countBits_v2(n: int) -> List[int]:
     return [bin(i).count("1") for i in range(n+1)]
 
+
+# The number of 1s in any bit value will be equal to the number of 1s to any number bitshifted right once, not including the least significant digit. This can be determined
+# by checking if number is even or odd, adding 1 for odd numbers.
+# e.g. 1110011 bitshifted is 111001 + 1
+# That means as we go up the number list, we already have most of our answers previously in the list. No need for the ln part of O(nlgn)
+def countBits_memo(n: int) -> List[int]:
+    memo = [0 for _ in range(n+1)]
+    for i in range(1,n+1):
+        memo[i] = memo[i >> 1] + i % 2
+    return memo
+
 def convert_int_to_binary_list(i: int) -> int:
     # init array of binary to appropriate length
     result = [0 for _ in range(math.floor(math.log2(i))+1)]
@@ -37,3 +48,4 @@ def convert_int_to_binary_list_v2(i: int) -> int:
     return result[::-1] # Missy Elliott
 
 
+print(countBits(40))
--- tests/ch2/test_countBits.py
+++ tests/ch2/test_countBits.py
@@ -17,6 +17,14 @@ class TestCountBitsV2(unittest.TestCase):
     def test8(self):
         self.assertEqual(countBits_v2(8),[0,1,1,2,1,2,2,3,1])
 
+class TestCountBitsMemo(unittest.TestCase):
+    def test0(self):
+        self.assertEqual(countBits_memo(0),[0])
+    def test2(self):
+        self.assertEqual(countBits_memo(2),[0,1,1])
+    def test8(self):
+        self.assertEqual(countBits_memo(8),[0,1,1,2,1,2,2,3,1])
+
 class TestConvertVal(unittest.TestCase):
     def test4(self):
         self.assertEqual(convert_int_to_binary_list(4),[1,0,0])

4f1b07225d7231b69c92c70e02773c819d1776ca
Jared Wolterstorff <jstorff@gmail.com>
2023-07-26 00:41:43 -0500

    count_smaller v1

--- src/ch3/trees.py
+++ src/ch3/trees.py
@@ -276,3 +276,55 @@ b) O(n) space with O(lg n) time screams "tree!". After a big of digging, I have 
    Since we don't have to maintain this or deal with insert/delete operations (although we could and I should build that...),
    We can implement this as a regular BST that's balanced at initialization then just store and use this static structure.
 """
+
+
+
+"""
+Given an integer array nums, return an integer array counts where counts[i] is the number of smaller elements to the right of nums[i].
+"""
+
+class CountTreeNode:
+    def __init__(self, val, left = None, right = None) -> None:
+        self.val = val
+        self.num_left = 0
+        self.left = left
+        self.right = right
+    
+    def insert(self,val,count = 0) -> int:
+        if self is None:
+            self = CountTreeNode(val)
+            return count
+        if val < self.val:
+            if self.left is None:
+                self.num_left += 1 # We need to increment here to keep a count of left children for use by any nodes added to right side later
+                self.left = CountTreeNode(val)
+                return count
+            else:
+                self.num_left += 1 # We need to increment here to keep a count of left children for use by any nodes added to right side later
+                return self.left.insert(val, count) # Pass in original count, not the incremented one
+        if val > self.val:
+            count += self.num_left + 1
+            if self.right is None:
+                self.right = CountTreeNode(val)
+                return count
+            else:
+                return self.right.insert(val, count)
+        if val == self.val: 
+            count += self.num_left
+            if self.right is None:
+                self.right = CountTreeNode(val)
+                return count
+            else:
+                return self.right.insert(val, count)
+
+def count_smaller(nums: List[int]) -> List[int]:
+    root = CountTreeNode(nums.pop())
+    result = [0]
+    while nums:
+        val = nums.pop()
+        result.append(root.insert(val))
+    
+    result.reverse()
+    return result
+
+print(count_smaller([3,1,4,2]))
